fd00c07949d281ddc4c514fb6f0139a9062f65a4
vibhore tiwari <vibhore.tiwari@coraltele.com>
2024-09-24 10:50:45 +0530

    initaialize project

---
+++ facedetectioscalaclient/src/main/java/com/example/facedetectioscalaclient/FacedetectioscalaclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.facedetectioscalaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacedetectioscalaclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacedetectioscalaclientApplication.class, args);
+	}
+
+}
---
+++ facedetectioscalaclient/src/test/java/com/example/facedetectioscalaclient/FacedetectioscalaclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facedetectioscalaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacedetectioscalaclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
